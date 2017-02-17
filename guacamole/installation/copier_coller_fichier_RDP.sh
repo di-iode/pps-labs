4067c1429d28dd5678e8a75ebe8913e96dda65b9
di-iode <fbbere@gmail.com>
2017-02-17 12:02:53 +0100

    Update copier_coller_fichier_RDP.sh

--- guacamole/installation/copier_coller_fichier_RDP.sh
+++ guacamole/installation/copier_coller_fichier_RDP.sh
@@ -4,8 +4,9 @@ sudo su
 ##recuperation des fichiers et creation des liens symboliques necessaires
 mkdir /usr/lib/x86_64-linux-gnu/freerdp/
 
+## on cherche les fichiers existant en retirant ceux dans le /home
 lien1=$(find / -name guacdr-client.so | grep -v home)
 lien2=$(find / -name guacsnd-client.so | grep -v home)
 
 ln -s $lien1 /usr/lib/x86_64-linux-gnu/freerdp/guacdr-client.so
-ln -s $lien1 /usr/lib/x86_64-linux-gnu/freerdp/guacsnd-client.so
+ln -s $lien2 /usr/lib/x86_64-linux-gnu/freerdp/guacsnd-client.so
